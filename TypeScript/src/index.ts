9ac8f1d4bfba51a909ac37c10f4df0aeb292f1c0
Francisco Larrosa <franciscolarrosa22@gmail.com>
2022-02-15 23:36:04 -0300

    TypeScript

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,7 +1,20 @@
-import { producto,calcularISV } from './ejercicios/06-desestructuracion-funcion';
+import { producto, calcularISV } from './ejercicios/06-desestructuracion-funcion';
 
 /*
 ===== Código de TypeScript =====
 
 */
 
+class Hero {
+
+    alterEgo: string;
+    edad: number;
+    nombreReal: string;
+}
+
+// STATIC es paraa acceder sin crear una instancia
+
+
+const ironman = new Hero();
+
+console.log(ironman);
