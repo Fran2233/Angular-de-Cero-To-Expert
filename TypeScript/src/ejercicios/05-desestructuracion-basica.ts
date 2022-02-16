3bb82c144e47a1a2a5a63f782204ef76190fc33d
Francisco Larrosa <franciscolarrosa22@gmail.com>
2022-02-15 22:38:06 -0300

    TypeScript

---
+++ TypeScript/src/ejercicios/05-desestructuracion-basica.ts
@@ -0,0 +1,45 @@
+
+/*
+===== Código de TypeScript =====
+
+*/
+interface reproductor {
+    volumen: number,
+    segundo: number,
+    cancion: string,
+    detalles: detalles
+}
+
+interface detalles {
+    autor: string,
+    anio: number;
+}
+
+const reproductor: reproductor = {
+    volumen: 100,
+    segundo: 36,
+    cancion: 'Mess',
+    detalles: {
+        autor: 'Ed Sheeran',
+        anio: 2022,
+    }
+}
+
+// Desestructuracion de OBJETOS
+
+const { volumen, segundo, cancion, detalles/* , detalles:{autor :autorDetalles ,anio: anioDetalles} */ } = reproductor; /* Todo en una linea */
+const { autor, anio } = detalles;  /* --> mejor usa esta */
+
+// console.log('El volumen actual es de: ', volumen)
+// console.log('El segundo actual es de: ', segundo)
+// console.log('La cancion actual es : ', cancion)
+// console.log('El Autor actual es de: ', autor)
+// console.log('El anio actual es de: ', anio)
+
+
+const dbz: string[] = ['Goku', 'Vegeta', 'Trunks'];
+
+const [, , p3] = dbz;
+
+
+console.log(p3);
