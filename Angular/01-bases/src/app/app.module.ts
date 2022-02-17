323b88298004fcc9a8a037307adde1d919857559
Francisco Larrosa <franciscolarrosa22@gmail.com>
2022-02-16 23:12:45 -0300

    Angular

--- Angular/01-bases/src/app/app.module.ts
+++ Angular/01-bases/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { ContadorModule } from './contador/contador.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
     
   ],
   providers: [],
