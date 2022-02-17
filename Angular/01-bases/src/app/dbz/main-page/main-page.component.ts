08a2d99aaadebdb3a42b761dcdc7f858d78ab0b3
Francisco Larrosa <franciscolarrosa22@gmail.com>
2022-02-17 10:41:12 -0300

    Angular

--- Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
+++ Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -7,7 +7,6 @@ import { Personaje } from '../interfaces/dbz.interface';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
 
 
@@ -28,23 +27,12 @@ export class MainPageComponent {
   ];
 
   nuevo: Personaje = {
-    nombre: '',
-    poder: 0
-  }
-
-  agregar() {
-
-    if (this.nuevo.nombre.trim().length === 0) {
-      return;
-    }
-    console.log(this.nuevo);
-    this.personajes.push(this.nuevo);
-    this.nuevo = {
-      nombre: '',
-      poder: 0
-    }
+    nombre: 'ROSHI',
+    poder: 1000
 
   }
+
+ 
 
 
 
